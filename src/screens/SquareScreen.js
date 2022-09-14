40287e4d8e93f80934d9f96c32a081a6f62fca25
Mario Posada <marioposadamiranda@gmail.com>
2022-09-14 09:57:31 -0300

    New componente

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,16 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+import React from 'react'
+
+const SquareScreen =()=> {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+const styles=  StyleSheet.create({});
+
+export default SquareScreen;
+
